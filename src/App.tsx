533c160ddf49402d38450920329aa389cc09c6a6
Cristi Mihai <cristi.mihai@iplicit.com>
2025-08-05 15:50:09 +0100

    tweaks; diff

--- src/App.tsx
+++ src/App.tsx
@@ -2,26 +2,46 @@ import { useState } from "react";
 import "./styles.css";
 
 export default function App() {
-  const [stat, setStat] = useState(`
-  (1 row affected)
-Table 'Worktable'. Scan count 3, logical reads 122, physical reads 0, page server reads 0, read-ahead reads 0, page server read-ahead reads 0, lob logical reads 20, lob physical reads 0, lob page server reads 0, lob read-ahead reads 0, lob page server read-ahead reads 0.
-`);
+  const [stat1, setStat1Base] = useState(() => localStorage.getItem("stat1") ?? `Table '[dbo].[example]'. Scan count 1, logical reads 2403, physical reads 0, page server reads 0, read-ahead reads 0, page server read-ahead reads 0, lob logical reads 0, lob physical reads 0, lob page server reads 0, lob read-ahead reads 0, lob page server read-ahead reads 0.
 
-  const cols = [];
+(100 rows affected)`);
+  const setStat1 = (v: string) => {
+    localStorage.setItem("stat1", v);
+    setStat1Base(v);
+  };
 
-  const totals = [];
-  const totalsKb = [];
+  const [stat2, setStat2Base] = useState(() => localStorage.getItem("stat2") ?? "");
+  const setStat2 = (v: string) => {
+    localStorage.setItem("stat2", v);
+    setStat2Base(v);
+  };
 
-  const d = stat
+  const cols = [
+    "Scan count", "Logical reads", "Physical reads"
+  ];
+  const tables = [];
+  const rows = { 'Totals': [], 'Totals (KB)': []};
+  const totals = rows['Totals'];
+  const totalsKb = rows['Totals (KB)'];
+  const keys = [];
+  
+  function parseStats(s: string, key: string) {
+
+    if (!keys.includes(key)) keys.push(key);
+
+    s
     .split("\n")
     .filter((i) => i.startsWith("Table "))
-    .map((i) => {
+    .forEach((i) => {
       // Table 'currency'. Scan count 0, logical reads 2, physical reads 0, page server reads 0, read-ahead reads 0, page server read-ahead reads 0, lob logical reads 0, lob physical reads 0, lob page server reads 0, lob read-ahead reads 0, lob page server read-ahead reads 0.
-      const s = /^Table '(\w+)'. (.*)$/.exec(i);
+      const s = /^Table '(.*)'. (.*)$/.exec(i);
       if (!s) return;
       const t = s[1];
       const r = s[2];
-      const vals = [];
+
+      const row = rows[t] || (rows[t] = {});
+
+      if (!tables.includes(t)) tables.push(t);
 
       for (let i of r.split(",")) {
         const x = /^\s*(.*) (\d+)$/.exec(i);
@@ -29,52 +49,89 @@ Table 'Worktable'. Scan count 3, logical reads 122, physical reads 0, page serve
         let [_, t, vs] = x;
 
         let v = parseInt(vs);
+        if (!v && t != "Scan count") continue
 
-        let ix = cols.indexOf(t);
+        let tt = t.trim();
+        tt = tt[0].toUpperCase() + tt.substring(1);
+        let ix = cols.indexOf(tt);
         if (ix == -1) {
-          cols.push(t);
+          cols.push(tt);
           ix = cols.length - 1;
         }
-        vals[ix] = v;
+        const rowi = row[ix] || (row[ix] = {});
+        rowi[key] = v;
 
-        totals[ix] = (totals[ix] || 0) + v;
-
-        if (t.endsWith("reads")) totalsKb[ix] = (totalsKb[ix] || 0) + v * 8;
+        totals[ix] ??= {};
+        totalsKb[ix] ??= {};
+        totals[ix][key] = (totals[ix][key] || 0) + v;
+        if (t != "Scan count") totalsKb[ix][key] = (totalsKb[ix][key] || 0) + v * 8;
       }
+    });
+  }
 
-      return { table: t, vals };
-    })
-    .filter((i) => i);
+  parseStats(stat1, "stat1");
 
-  console.log(d);
+  if (stat2)
+    parseStats(stat2, "stat2");
+
+  tables.push("Totals");
+  tables.push('Totals (KB)');
+
+  console.log(rows);
+
+  const key0 = keys[0];
 
   return (
     <div className="App">
-      <textarea onChange={(ev) => setStat(ev.currentTarget.value)}>
-        {stat}
-      </textarea>
+      <div style={{ display: "flex", gap: "1rem" }}>
+        <textarea onChange={(ev) => setStat1(ev.currentTarget.value)} placeholder="Paste here the results of a query wrapped in SET STATISTICS IO ON">
+          {stat1}
+        </textarea>
+        <textarea onChange={(ev) => setStat2(ev.currentTarget.value)} placeholder="Paste another query results here to compare">
+          {stat2}
+        </textarea>
+      </div>
 
-      <table border="1">
+      <table border={1}>
         <thead>
           <tr>
             <td></td>
             {cols.map((i) => (
-              <td>{i}</td>
+              <td colSpan={keys.length}>{i}</td>
             ))}
           </tr>
         </thead>
         <tbody>
-          {d.map((i, k) => (
-            <tr key={k}>
-              <td>{i.table}</td>
-              {cols.map((c, k) => (
-                <td className="ar">{fmt(i.vals?.[k])}</td>
-              ))}
-            </tr>
-          ))}
+          {tables.map((t, k) => {
+            const r = rows[t];
+            return (
+              <tr key={k} className={t.startsWith("Total") ? "total" : ""}>
+                <td>{t}</td>
+                {cols.map((c, ck) => 
+                  keys.map((key, ki) => {
+                    const v = r?.[ck]?.[key];
+                    let cls = '';
+                    if (ki > 0) {
+                      const b0 = r?.[ck]?.[key0];
+                      if (!b0 && v) cls = `new`;
+                      else if (v > b0) cls = `inc`;
+                      else if (v < b0) cls = `dec`;
+                    }
+                    return (
+                      <td className={`ar val val-${ki}`}>
+                        <div className={cls}>
+                          {fmt(v)}
+                        </div>
+                      </td>
+                    );
+                  })
+                )}
+              </tr>
+            );
+          })}
         </tbody>
 
-        <tfoot>
+        {/* <tfoot>
           <tr>
             <td>Total</td>
             {cols.map((i, k) => (
@@ -87,7 +144,8 @@ Table 'Worktable'. Scan count 3, logical reads 122, physical reads 0, page serve
               <td className="ar">{fmt(totalsKb[k])}</td>
             ))}
           </tr>
-        </tfoot>
+        </tfoot> */}
+
       </table>
     </div>
   );
